955da3538d570468ebd74b7d39a99375a03e5b56
scurtis7 <scurtis7@gmail.com>
2019-12-22 15:58:25 -0500

    change spring boot application name

--- src/main/java/com/scurtis/roster/Application.java
+++ src/main/java/com/scurtis/roster/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FsuRosterApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FsuRosterApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
