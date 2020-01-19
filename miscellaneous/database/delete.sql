b984f195b79afccb5a612d7860a63be96a9cfdbc
scurtis7 <scurtis7@gmail.com>
2020-01-18 20:54:24 -0500

    update init and delete sql files

--- miscellaneous/database/delete.sql
+++ miscellaneous/database/delete.sql
@@ -1,19 +1,25 @@
 -- Delete the FSU Roster database
 
-ALTER TABLE IF EXISTS fsu_roster.recruit
-DROP CONSTRAINT IF EXISTS recruit_pkey;
+ALTER TABLE IF EXISTS fsu_roster.coach
+DROP CONSTRAINT IF EXISTS coach_pkey CASCADE;
 
 ALTER TABLE IF EXISTS fsu_roster.recruit
-DROP CONSTRAINT IF EXISTS recruit_playerid_fkey;
+DROP CONSTRAINT IF EXISTS recruit_pkey CASCADE;
+
+ALTER TABLE IF EXISTS fsu_roster.recruit
+DROP CONSTRAINT IF EXISTS recruit_playerid_fkey CASCADE;
 
 ALTER TABLE IF EXISTS fsu_roster.player
-DROP CONSTRAINT IF EXISTS player_pkey;
+DROP CONSTRAINT IF EXISTS player_pkey CASCADE;
 
+DROP SEQUENCE IF EXISTS fsu_roster.coach_coachid_seq CASCADE;
 DROP SEQUENCE IF EXISTS fsu_roster.recruit_recruitid_seq CASCADE;
 DROP SEQUENCE IF EXISTS fsu_roster.player_playerid_seq CASCADE;
 
-DROP TABLE IF EXISTS fsu_roster.coach;
-DROP TABLE IF EXISTS fsu_roster.player;
-DROP TABLE IF EXISTS fsu_roster.recruit;
+DROP FUNCTION IF EXISTS fsu_roster.trigger_set_timestamp() CASCADE;
+
+DROP TABLE IF EXISTS fsu_roster.coach CASCADE;
+DROP TABLE IF EXISTS fsu_roster.recruit CASCADE;
+DROP TABLE IF EXISTS fsu_roster.player CASCADE;
 
 DROP SCHEMA IF EXISTS fsu_roster;
