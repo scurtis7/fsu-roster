84e7ab0eec6705077530bc474ede7765e8422a57
scurtis7 <scurtis7@gmail.com>
2020-01-26 12:49:09 -0500

    add delete coach functionality

--- src/main/java/com/scurtis/roster/controller/CoachRestController.java
+++ src/main/java/com/scurtis/roster/controller/CoachRestController.java
@@ -7,7 +7,9 @@ import com.scurtis.roster.model.coach.CoachRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,5 +44,12 @@ public class CoachRestController {
         coachRepository.save(coach);
     }
 
+    @DeleteMapping(value = "/coach/{id}")
+    public List<Coach> deleteCoach(@PathVariable(value = "id") Long id) {
+        log.info("Method: deleteCoach('{}')", id);
+        Coach coach = coachRepository.getOne(id);
+        coachRepository.delete(coach);
+        return coachRepository.findAll();
+    }
 
 }
--- src/webapp/src/coach/Coaches.js
+++ src/webapp/src/coach/Coaches.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import "./Coaches.css"
 import axios from 'axios';
 import {Table} from 'reactstrap';
+import Button from "reactstrap/es/Button";
 
 class Coaches extends Component {
 
@@ -12,10 +13,17 @@ class Coaches extends Component {
         };
         this.setCoaches = this.setCoaches.bind(this);
         this.loadTableData = this.loadTableData.bind(this);
+        this.deleteCoach = this.deleteCoach.bind(this);
     }
 
     setCoaches(coaches) {
         this.setState({coaches});
+    }
+
+    deleteCoach(id) {
+        axios.delete('http://localhost:8080/api/coach/' + id)
+            .then(response => this.setCoaches(response.data))
+            .catch(error => error);
     }
 
     loadTableData() {
@@ -32,6 +40,10 @@ class Coaches extends Component {
                         <td>{name}</td>
                         <td>{position}</td>
                         <td>{sport}</td>
+                        <td>
+                            <Button className="Player-button" classActiveName="Player-button-active" onClick={() => this.deleteCoach(coachId)}>Delete</Button>
+                            <Button className="Player-button" classActiveName="Player-button-active">Edit</Button>
+                        </td>
                     </tr>
                 );
             });
@@ -39,7 +51,7 @@ class Coaches extends Component {
     }
 
     componentDidMount() {
-        axios(`http://localhost:8080/api/coaches`)
+        axios('http://localhost:8080/api/coaches')
             .then(coaches => this.setCoaches(coaches.data))
             .catch(error => error);
     }
@@ -54,6 +66,7 @@ class Coaches extends Component {
                         <th>Name</th>
                         <th>Position</th>
                         <th>Sport</th>
+                        <th>Actions</th>
                     </tr>
                     </thead>
                     <tbody>
