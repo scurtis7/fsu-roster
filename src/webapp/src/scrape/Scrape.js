8f0c5bb68a3ce84145140373e30dac3ab79c70e3
scurtis7 <scurtis7@gmail.com>
2020-01-31 18:15:46 -0500

    initial coach scraping

--- src/webapp/src/scrape/Scrape.js
+++ src/webapp/src/scrape/Scrape.js
@@ -1,18 +1,37 @@
 import React, {Component} from "react";
 import "./Scrape.css"
-import {Container, Row, Badge} from 'reactstrap';
+import {Container, Row, Col, Button, ModalFooter} from 'reactstrap';
+import axios from "axios";
 
 class Scrape extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+        this.scrapeCoaches = this.scrapeCoaches.bind(this);
+        this.loadResults = this.loadResults.bind(this);
+    }
+
+    scrapeCoaches() {
+        axios.get('http://localhost:8080/api/scrape/coach/')
+            .then(() => {
+                this.loadResults();
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    loadResults() {
+    }
+
     render() {
         return (
             <div className="Scrape">
                 <Container fluid>
                     <Row>
-                        <h1><Badge className="Scrape-badge">
-                            Scrape Page
-                        </Badge></h1>
-                    </Row>
-                    <Row>
+                        <Button className="Scrape-button" classActiveName="Scrape-button-active" onClick={this.scrapeCoaches}>Get Coaches</Button>
                     </Row>
                 </Container>
             </div>
