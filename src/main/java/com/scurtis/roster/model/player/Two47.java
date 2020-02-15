effe7cfb113c963b91e5cf587e5ac0441a3c592b
scurtis7 <scurtis7@gmail.com>
2020-02-15 14:01:24 -0500

    cleanup

--- src/main/java/com/scurtis/roster/model/player/Two47.java
+++ src/main/java/com/scurtis/roster/model/player/Two47.java
@@ -3,15 +3,11 @@ package com.scurtis.roster.model.player;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
